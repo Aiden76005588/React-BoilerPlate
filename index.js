74fc90f7139d313eb685e27b006fe64d0822dd8c
Aiden76005588 <fsd76005588@gmail.com>
2021-04-28 08:37:10 +0900

    [server] 비밀정보보호

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.user(express.json());
 
 const mongoose = require("mongoose");
-
 mongoose
   .connect(
     //.env명령어로 실행하면 오류가 뜬다. 왜그럴까? 위에서 config설정 다해줬는데...
-    "mongodb+srv://hyunkyu:hyunkyu1234@cluster0.vpbup.mongodb.net/gractor?retryWrites=true&w=majority",
+    config.mongoURI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -19,4 +27,15 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.post("/register", (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.listen(port, () => console.log(`Exmple app listening on port ${port}!`));
