6de81262bc56e3753287edbf25327061c758829d
Aiden76005588 <fsd76005588@gmail.com>
2021-04-29 13:10:01 +0900

    [server] Login Router

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -9,6 +12,10 @@ const userSchema = mongoose.Schema({
     type: String,
     trim: true,
     unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
   },
   lastname: {
     type: String,
@@ -27,6 +34,44 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.pre("save", function (next) {
+  //비밀번호를 암호화 시킨다.
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 123456 암호화된 비밀번호
+  // console.log("asdbaksjbdkjabskjdb", plainPassword, this.password);
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  //user._id +'secretToken' = token
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err); //<--에러가 생기면 콜백으로 에러전달
+    cb(null, user); //<--에러가 없다면(null)이라면 user 정보만 전달 -->index의 generateToken 함수로 간다.
+  });
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
